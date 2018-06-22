d7a3e4de6ae36efa14b8b99f4ecb01bec9e5195d
thela <thela@LAPTOP-U3VKHU3J.local>
2018-06-22 07:46:00 -0400

    Premier commite

---
+++ GitTestProject/src/Affichage.java
@@ -0,0 +1,8 @@
+
+
+public class Affichage {
+
+	public void print() {
+		System.out.println("JE AFFICHE ICI");
+	}
+}
---
+++ GitTestProject/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Affichage out = new Affichage();
+		out.print();
+	}
+
+}
